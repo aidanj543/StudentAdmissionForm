3239d4741b1635435e5b4f28cd24bc6ea39c0062
Aidan Jaramillo <aidanjaramillo0326@gmail.com>
2026-03-05 17:33:51 -0600

    Made string lower and tested php

--- emailcreator.php
+++ emailcreator.php
@@ -11,4 +11,18 @@ $yearLastTwoDigits = substr($year, -2);
 $firstPart = substr($first, 0, 4);
 $lastPart = substr($last, 0, 4);
 
-$email = $department."_".$firstPart. $lastPart.$yearLastTwoDigits."@jbu.edu";
+$email = strtolower($department."_".$firstPart. $lastPart.$yearLastTwoDigits."@jbu.edu");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Email Created</title>
+        <link rel = "stylesheet" href = "styles.css">
+    </head>
+    <body>
+        <h1>Email Created!</h1>
+        <p>Your email address is: <?php echo $email; ?></p>
+    </body>
+</html>
